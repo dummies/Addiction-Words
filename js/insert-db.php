449c5774f8dcc6a65de8c5e93613ec107f1b4eea
dummies <saisreenivasan@live.in>
2012-12-26 18:01:08 +0530

    hope

--- js/insert-db.php
+++ js/insert-db.php
@@ -1,33 +1,28 @@
 <?php
-    // DB connection info
-    //TODO: Update the values for $host, $user, $pwd, and $db
-    //using the values you retrieved earlier from the portal.
-    $host = "us-cdbr-azure-east-b.cloudapp.net";
+	
+	$host = "us-cdbr-azure-east-b.cloudapp.net";
     $user = "bcd2949c8baf7b";
     $pwd = "ee7246b9";
     $db = "wordaddABbVVe2ev";
-  
-$con = mysql_connect("mysql:host=$host;dbname=$db",$user,$pwd);
-if (!$con)
-  {
-  die('Could not connect: ' . mysql_error());
-  }
-$sql = "CREATE TABLE score_card(name VARCHAR(30), score int)";
-if (!mysql_query($sql,$con))
-  {
-  die('Error: ' . mysql_error());
-  } 
-mysql_select_db("score_card", $con);
- $name = $_GET["name"];
- $score = $_GET["score"]; 
-$sql="INSERT INTO score_card(name,score) VALUES($name,$score)";
-if (!mysql_query($sql,$con))
-  {
-  die('Error: ' . mysql_error());
-  }
-echo "1 record added";
-
-mysql_close($con);
-   
- 
+    try 
+	{
+        $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
+        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    }
+    catch(Exception $e)
+	{
+        die(var_dump($e));
+    }
+     $sql1 = "CREATE TABLE scoreboard( name VARCHAR(30), score int)";
+	 $conn->query($sql1);
+        $name = $_GET["name"];
+        $score = $_GET["score"];
+        // Insert data
+        $sql_insert = "INSERT INTO scoreboard (name, score) 
+                   VALUES (?,?)";
+        $stmt = $conn->prepare($sql_insert);
+        $stmt->bindValue(1, $name);
+        $stmt->bindValue(2, $score);
+        $stmt->execute();
+		echo "<h3>Data entered!</h3>";
 ?>
