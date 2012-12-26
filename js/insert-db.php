9338b4dd99f09bbe1170856f71e390b5c5094b74
dummies <saisreenivasan@live.in>
2012-12-26 17:18:07 +0530

    chnage

--- js/insert-db.php
+++ js/insert-db.php
@@ -6,36 +6,21 @@
     $user = "bcd2949c8baf7b";
     $pwd = "ee7246b9";
     $db = "wordaddABbVVe2ev";
-
-
-    // Connect to database.
-    try 
-	{
-        $con = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
-        $con->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-    }
-    catch(Exception $e)
-	{
-        die(var_dump($e));
-    }
-	//comment here once table created.
- $sql = "CREATE TABLE score_card(name VARCHAR(30), score int)"; 
   
-  
-  //$con = mysql_connect("localhost","peter","abc123");
+$con = mysql_connect("mysql:host=$host;dbname=$db",$user,$pwd);
 if (!$con)
   {
   die('Could not connect: ' . mysql_error());
   }
+$sql = "CREATE TABLE score_card(name VARCHAR(30), score int)";
+if (!mysql_query($sql,$con))
+  {
+  die('Error: ' . mysql_error());
+  } 
 mysql_select_db("score_card", $con);
-
-
-  $name = $_GET["name"];
-  $score = $_GET["score"]; 
-$sql="INSERT INTO Persons (name,score)
-VALUES
-($name,$score)";
-
+ $name = $_GET["name"];
+ $score = $_GET["score"]; 
+$sql="INSERT INTO score_card(name,score) VALUES($name,$score)";
 if (!mysql_query($sql,$con))
   {
   die('Error: ' . mysql_error());
@@ -45,19 +30,4 @@ echo "1 record added";
 mysql_close($con);
    
  
-/* $sql_insert = "INSERT INTO _scoreboard(name, score) VALUES (?,?)";
-  $stmt = $conn->prepare($sql_insert);
-  $stmt->bindValue(0, $name);
-  $stmt->bindValue(1, $score);
-print '<script type="text/javascript">'; 
-  print 'alert("at 2");
-  print '</script>';  
-        $stmt->execute();
-   print '<script type="text/javascript">'; 
-  print 'alert("at 5");
-    print '</script>';  
-  print '<script type="text/javascript">'; 
-  print 'alert("inserted") ';
-  print '</script>'; 
-   echo 'success';	*/
 ?>
