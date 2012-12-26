241227366e36158b33cf2c7f41d94579348c58c0
dummies <saisreenivasan@live.in>
2012-12-26 18:34:31 +0530

    jhusuasj

--- js/rank.php
+++ js/rank.php
@@ -1,7 +1,20 @@
 <html>
 <body>
 <?php 
-echo 'website';
+$host = "us-cdbr-azure-east-b.cloudapp.net";
+    $user = "bcd2949c8baf7b";
+    $pwd = "ee7246b9";
+    $db = "wordaddABbVVe2ev";
+    try 
+	{
+        $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
+        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    }
+    catch(Exception $e)
+	{
+        die(var_dump($e));
+    }
+echo 'results';
  $sql_select = "SELECT * FROM scoreboard order by score desc";
     $stmt = $conn->query($sql_select);
     $registrants = $stmt->fetchAll(); 
