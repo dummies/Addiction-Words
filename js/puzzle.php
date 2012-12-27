7a2edde12357c6e274208b7f25f2c3de0573c499
dummies <saisreenivasan@live.in>
2012-12-27 16:15:43 +0530

    advanced

--- js/puzzle.php
+++ js/puzzle.php
@@ -2,15 +2,12 @@
 $puzzle = "";
 $vowels=array("A","E","I","O","U");
 $conso =array("B","C","D","F","G","H","J","K","L","M","N","P","Q","R","S","T","V","W","X","Y","Z");
-for($i=0; $i <15;)
+$list =array('E','E','E','E','E','E','T','T','T','A','A','A','O','O','O','I','I','I','N','N','N','S','S','S','S','R','R','R','H','H','L','L','D','D','D','D','C','C','U','U','M','M','F','P','G','W','Y','B','V','K','X','J','Q','Z');
+$len  =strlen($list)-1;
+for($i=0; $i <15;$i++)
 {	
-	$puzzle .= $conso[rand(0,20)];
-	$puzzle .= $vowels[rand(0,4)];
-	$puzzle .= $conso[rand(0,20)];
-	$i = $i +3;
+	$puzzle .= $list[rand(0,$len)];
 }
-
-$puzzle .= $vowels[rand(0,4)];
 //completed generation 
 
 echo $puzzle;
