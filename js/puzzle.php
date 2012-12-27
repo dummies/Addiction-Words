c633751b852527a9d6667cd513cba0270e8fbc29
dummies <saisreenivasan@live.in>
2012-12-27 16:26:25 +0530

    bjbxs

--- js/puzzle.php
+++ js/puzzle.php
@@ -1,12 +1,24 @@
 <?php
 $puzzle = "";
-$vowels=array("A","E","I","O","U");
-$conso =array("B","C","D","F","G","H","J","K","L","M","N","P","Q","R","S","T","V","W","X","Y","Z");
-$list =array('E','E','E','E','E','E','T','T','T','A','A','A','O','O','O','I','I','I','N','N','N','S','S','S','S','R','R','R','H','H','L','L','D','D','D','D','C','C','U','U','M','M','F','P','G','W','Y','B','V','K','X','J','Q','Z');
-$len  =strlen($list)-1;
-for($i=0; $i <15;$i++)
+//$vowels=array("A","E","I","O","U");
+//$conso =array("B","C","D","F","G","H","J","K","L","M","N","P","Q","R","S","T","V","W","X","Y","Z");
+//$list =array('E','E','E','E','E','E','T','T','T','A','A','A','O','O','O','I','I','I','N','N','N','S','S','S','S','R','R','R','H','H','L','L','D','D','D','D','C','C','U','U','M','M','F','P','G','W','Y','B','V','K','X','J','Q','Z');
+
+
+$high=array("E","T","A","O","I","N");
+$medium=array("S","R","H","L","D","C","U","M","F","P");
+$low=array("G","W","Y","B","V","K","X","J","Q","Z");
+$len1=strlen($high)-1;
+$len2=strlen($medium)-1;
+$len3=strlen($low)-1;
+//$len  =strlen($list)-1;
+for($i=0; $i <16;)
 {	
-	$puzzle .= $list[rand(0,$len)];
+	$puzzle .= $high[rand(0,$len1)];
+	$puzzle .= $medium[rand(0,$len2)];
+	$puzzle .= $low[rand(0,$len3)];
+	$puzzle .= $high[rand(0,$len1)];
+	$i=$i+4;
 }
 //completed generation 
 
