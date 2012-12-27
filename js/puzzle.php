44424f6fee0a0a5a547acf32bcc9cd82ab2db38a
dummies <saisreenivasan@live.in>
2012-12-27 16:35:18 +0530

    hope 2 -@sai

--- js/puzzle.php
+++ js/puzzle.php
@@ -14,10 +14,10 @@ $len3=strlen($low)-1;
 //$len  =strlen($list)-1;
 for($i=0; $i <16;)
 {	
-	$puzzle .= $high[mt_rand()% $len1];
-	$puzzle .= $medium[mt_rand()% $len2];
-	$puzzle .= $low[mt_rand()% $len3];
-	$puzzle .= $high[mt_rand()% $len1];
+	$puzzle .= $high[mt_rand()% 6];
+	$puzzle .= $medium[mt_rand()% 10];
+	$puzzle .= $low[mt_rand()% 10];
+	$puzzle .= $high[mt_rand()% 6];
 	$i=$i+4;
 }
 //completed generation 
