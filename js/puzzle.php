e18de77ea752da00e186de0945ee19f1694d0a6d
dummies <saisreenivasan@live.in>
2012-12-27 14:30:05 +0530

    huh

--- js/puzzle.php
+++ js/puzzle.php
@@ -1,12 +1,17 @@
 <?php
 $puzzle = "";
-$i=0;
-
-for($i=0; $i <16; $i++)
-{
-	$ch = rand(65,90);
-	$puzzle .= chr($ch);
+$vowels=array("A","E","I","O","U");
+$conso =array("B","C","D","F","G","H","J","K","L","M","N","P","Q","R","S","T","V","W","X","Y","Z");
+for($i=0; $i <15;)
+{	
+	$puzzle .= $conso[rand(0,20)];
+	$puzzle .= $vowels[rand(0,4)];
+	$puzzle .= $conso[rand(0,20)];
+	$i = $i +3;
 }
+
+$puzzle .= $vowels[rand(0,4)];
+//completed generation 
 
 echo $puzzle;
 
