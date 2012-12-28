2136252cfd2825016feaea88b85b9e0efb68697b
dummies <saisreenivasan@live.in>
2012-12-28 13:35:25 +0530

    hmm umm

--- js/dbgamepush.php
+++ js/dbgamepush.php
@@ -7,8 +7,7 @@
 
 <body>
 <?php
-echo "dbpushe";
-
+    echo "dbpushe";
 	$host = "us-cdbr-azure-east-b.cloudapp.net";
     $user = "bcd2949c8baf7b";
     $pwd = "ee7246b9";
@@ -22,16 +21,18 @@ echo "dbpushe";
 	{
         die(var_dump($e));
     }
+	$list=array('E','E','E','E','E','E','T','T','T','A','A','A','O','O','O','I','I','I','N','N','N','S','S','S','S','R','R','R','H','H','L','L','D','D','D','D','C','C','U','U','M','M','F','P','G','W','Y','B','V','K','X','J','Q','Z');
 /*$sql1 = "CREATE TABLE games(id INT NOT NULL AUTO_INCREMENT, 
 			PRIMARY KEY(id),seq VARCHAR(16))";
 $conn->query($sql1);*/
+while(true) 
+{
 $sql3 = "TRUNCATE TABLE games";
 $conn->query($sql3);
-$list=array('E','E','E','E','E','E','T','T','T','A','A','A','O','O','O','I','I','I','N','N','N','S','S','S','S','R','R','R','H','H','L','L','D','D','D','D','C','C','U','U','M','M','F','P','G','W','Y','B','V','K','X','J','Q','Z');
    try 
 	{
 		$j =0;
-		while($j <480) {
+		while($j <10) {
 		$puzzle ="";
 		for($i=0; $i <16;$i++)
 			$puzzle .= $list[mt_rand()%54];
@@ -48,28 +49,8 @@ $list=array('E','E','E','E','E','E','T','T','T','A','A','A','O','O','O','I','I',
     {
         die(var_dump($e));
     }
-	$sql_select = "SELECT * FROM games order by id";
-    $stmt = $conn->query($sql_select);
-    $registrants = $stmt->fetchAll(); 
-    if(count($registrants) > 0)
-	{
-        echo "<h2>People who are registered:</h2>";
-        echo "<table>";
-        echo "<tr><th>ID</th>";
-        echo "<th>Sequence</th>";
-		$i=0;
-        foreach($registrants as $registrant) 
-		{
-            echo "<tr><td>".$registrant['id']."</td>";
-            echo "<td>".$registrant['seq']."</td>";
-        }
-        echo "</table>";
-    } 
-	else
-	{
-        echo "<h3>No one is currently registered.</h3>";
-    }
-
+	sleep(120);
+}
 ?>
 </body>
 </html>
