c0e31c999182f6445b1096a689572894e9c359ca
dummies <saisreenivasan@live.in>
2012-12-28 12:48:06 +0530

    bug

--- js/dbgamepush.php
+++ js/dbgamepush.php
@@ -22,25 +22,25 @@ echo 'dbpushe';
 	{
         die(var_dump($e));
     }
-$sql1 = "CREATE TABLE games(id INT NOT NULL AUTO_INCREMENT, 
+/*$sql1 = "CREATE TABLE games(id INT NOT NULL AUTO_INCREMENT, 
 			PRIMARY KEY(id),seq VARCHAR(16))";
-$conn->query($sql1);
+$conn->query($sql1);*/
 //$sql3 = "TRUNCATE TABLE games";
 //$conn->query($sql3);
-$puzzle = "";
 $list=array('E','E','E','E','E','E','T','T','T','A','A','A','O','O','O','I','I','I','N','N','N','S','S','S','S','R','R','R','H','H','L','L','D','D','D','D','C','C','U','U','M','M','F','P','G','W','Y','B','V','K','X','J','Q','Z');
    try 
 	{
-		$i =0;
-		while($i <480) {
+		$j =0;
+		while($j <480) {
 		$puzzle ="";
 		for($i=0; $i <16;$i++)
 			$puzzle .= $list[mt_rand()%54];
-		$sql_insert = "INSERT INTO scoreboard (seq) 
+		$sql_insert = "INSERT INTO games(seq) 
                    VALUES (?)";
         $stmt = $conn->prepare($sql_insert);
         $stmt->bindValue(1, $puzzle);
         $stmt->execute();
+		$j++;
 		}
         
     }
