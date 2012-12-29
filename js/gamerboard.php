bd7ffa9d07155374b11606014d3ff92490296399
dummies <saisreenivasan@live.in>
2012-12-29 20:07:32 +0530

    lets see

--- js/gamerboard.php
+++ js/gamerboard.php
@@ -22,7 +22,7 @@ var interval = setInterval(function() {
 
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<label style="font-size:24px;color:#408000;"  > next game : </label>
 <label id="timer_div" style="font:'Trebuchet MS', Arial, Helvetica, sans-serif; font-size:36px; color:#408000;position:relative ; left: +100px;"></label>
-
+<div>
  <img style="background-color:transparent;float:left" src="/images/stats.PNG" width="100" height="100">
 <br><br><br><label style="font-size:30px;color:#008055;"  > Gamer Statistics </label> </br></br></br>
 <p>
