5bd0c6c323a8c783cda50259c71bb5540687348c
dummies <saisreenivasan@live.in>
2012-12-30 15:01:42 +0530

    hmmms

--- js/fb.js
+++ js/fb.js
@@ -63,7 +63,7 @@ document.body.className = response.authResponse ? 'connected' : 'not_connected';
 function logoutUser() {  
 	FB.logout();
 	document.getElementById('user-info').style.display = 'block';
-	document.getElementById('user_photo').innerHTML="";
+	document.getElementById('user_photo').src="";
 	document.getElementById('user_name').innerHTML = 'Hi Player!';
 }
 function updateUserInfo(response) 
@@ -71,7 +71,8 @@ function updateUserInfo(response)
 	   console.log(response);
      FB.api('/me', function(response) {
        //document.getElementById('user-info').innerHTML = '<img src="https://graph.facebook.com/' + response.id + '/picture">' + 'hello ' + response.name;
-       document.getElementById('user_photo').innerHTML = '<img src="https://graph.facebook.com/' + response.id + '/picture">';
+       //document.getElementById('user_photo').innerHTML = '<img src="https://graph.facebook.com/' + response.id + '/picture">';
+	   document.getElementById('user_photo').src = "https://graph.facebook.com/"+response.id+"/picture";
        document.getElementById('user_name').innerHTML = 'Hi ,'+ response.first_name +'!';
 	   document.getElementById('user-info').style.display = 'none'
 	   console.log(response);
