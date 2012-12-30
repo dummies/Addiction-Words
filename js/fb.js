b860b3e179f53cf7a48c239b602cb0e11a013a6a
dummies <saisreenivasan@live.in>
2012-12-30 13:05:36 +0530

    image

--- js/fb.js
+++ js/fb.js
@@ -66,6 +66,7 @@ function updateUserInfo(response)
      FB.api('/me', function(response) {
        //document.getElementById('user-info').innerHTML = '<img src="https://graph.facebook.com/' + response.id + '/picture">' + 'hello ' + response.name;
 	   console.log(response);
+	   document.getElementById('photo').src="https://graph.facebook.com/' + response.id + '/picture";
 	   //store user details.
 	 window.localStorage.setItem("name",response.name);
 	 //window.localStorage.setItem("user_img_fb",response.id);
