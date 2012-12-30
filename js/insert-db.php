6c22718a0f87c9bdddcdbb1d85c2c1e11f518378
dummies <saisreenivasan@live.in>
2012-12-30 20:56:11 +0530

    newdb

--- js/insert-db.php
+++ js/insert-db.php
@@ -1,7 +1,6 @@
-
 <?php
 	/*used to insert data into leader board.*/
-	$host = "us-cdbr-azure-east-b.cloudapp.net";
+	/*$host = "us-cdbr-azure-east-b.cloudapp.net";
     $user = "bcd2949c8baf7b";
     $pwd = "ee7246b9";
     $db = "wordaddABbVVe2ev";
@@ -32,5 +31,31 @@
     catch(Exception $e)
     {
         die(var_dump($e));
+    }*/
+	try {
+    $conn = new PDO ( "sqlsrv:server = tcp:pvp6ee8yc7.database.windows.net,1433; Database = gamer_scores", "dummies", "dumm!es3");
+    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
     }
+   catch ( PDOException $e ) {
+   print( "Error connecting to SQL Server." );
+   die(print_r($e));
+}
+  $v1 = $_GET["name"];
+  $v2 = $_GET['score'];
+   
+   $stmt3 = $conn->prepare("INSERT INTO #scores (Name,Score) VALUES (:var1, :var2)");
+
+   if ($stmt3) {
+      $stmt3->bindValue(1, $v1);
+      $stmt3->bindValue(2, $v2);
+      if ($stmt3->execute())
+         echo "Execution succeeded\n";     
+      else
+         echo "Execution failed\n";
+   }
+   else
+      var_dump($conn->errorInfo());
+   
+
+
 ?>
--- js/rank.php
+++ js/rank.php
@@ -19,20 +19,20 @@
 <body>
 <?php  set_time_limit(0); ?>
  <img style="background-color:transparent;float:left" src="/images/board.PNG" width="100" height="100">
-<br><br><br><label style="font-size:30px;color:#7BA6B4;"  > Gamer Board </label> </br></br></br>
+<br><br><br><label style="font-size:30px;color:#008055;"  > Gamer Board </label> </br></br></br>
 <br/>
 <?php
    
     // DB connection info
     //TODO: Update the values for $host, $user, $pwd, and $db
     //using the values you retrieved earlier from the portal.
-   $host = "us-cdbr-azure-east-b.cloudapp.net";
+   /*$host = "us-cdbr-azure-east-b.cloudapp.net";
     $user = "bcd2949c8baf7b";
     $pwd = "ee7246b9";
-    $db = "wordaddABbVVe2ev";
+    $db = "wordaddABbVVe2ev";*/
     // Connect to database.
 	/*print '<script src="count.js"> </script>';*/
-    try 
+    /*try 
 	{
         $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
         $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION      );
@@ -40,23 +40,31 @@
     catch(Exception $e)
 	{
         die(var_dump($e));
-    }
+    }*/
 		/*$sql1 = "CREATE TABLE scoreboard( name VARCHAR(30), score int)";
 		$conn->query($sql1);*/
 		
     // Insert registration info
     
     // Retrieve data
+	try {
+    $conn = new PDO ( "sqlsrv:server = tcp:pvp6ee8yc7.database.windows.net,1433; Database = gamer_scores", "dummies", "dumm!es3");
+    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    }
+   catch ( PDOException $e ) {
+   print( "Error connecting to SQL Server." );
+   die(print_r($e));
+}
 	sleep(5);
-    $sql_select = "SELECT * FROM scoreboard order by score desc";
+    $sql_select = "SELECT * FROM scores order by score desc";
     $stmt = $conn->query($sql_select);
     $registrants = $stmt->fetchAll(); 
     if(count($registrants) > 0)
 	{
         echo "<table>";
-        echo "<tr><td style='color:#0000A0'>Rank</td>";
-        echo "<td style='color:#0000A0'>Gamer Name</td>";
-        echo "<td style='color:#0000A0'>Score</td></tr>";
+        echo "<tr><th color=#408000>Rank</th>";
+        echo "<th color=#408000>Gamer</th>";
+        echo "<th><th color=#408000>Score</th></th></tr>";
 		$i=0;
 		$prev=NULL;
         foreach($registrants as $registrant) 
@@ -66,9 +74,9 @@
 			$i=$i+1;
 			$prev=$registrant['score'];
 			}
-            echo "<tr><td style='color:#0000A0'>".$i."</td>";
-			echo "<td style='color:#0000A0'>".$registrant['name']."</td>";
-            echo "<td style='color:#0000A0'>".$registrant['score']."</td></tr>";
+            echo "<tr><th>".$i."</th>";
+			echo "<th>".$registrant['name']."</th>";
+            echo "<th><th>".$registrant['score']."</th></th></tr>";
         }
         echo "</table>";
     } 
