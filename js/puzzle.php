b8dce2d0c00c5b31c352fa358b463387e8dfcb16
dummies <saisreenivasan@live.in>
2012-12-31 19:41:38 +0530

    naive

--- js/puzzle.php
+++ js/puzzle.php
@@ -1,13 +1,16 @@
-<pre>
 <?php
-try {
-    $conn = new PDO ( "sqlsrv:server = tcp:pvp6ee8yc7.database.windows.net,1433; Database = gamer_scores", "dummies", "dumm!es3");
-    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+$host = "us-cdbr-azure-east-b.cloudapp.net";
+    $user = "bcd2949c8baf7b";
+    $pwd = "ee7246b9";
+    $db = "wordaddABbVVe2ev";
+    try 
+	{
+        $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
+        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
     }
-   catch ( PDOException $e ) 
-   {
-   print( "Error connecting to SQL Server." );
-   die(print_r($e));
+    catch(Exception $e)
+	{
+        die(var_dump($e));
     }
  $h =date('H');
  $m =date('i');
@@ -15,32 +18,25 @@ try {
  $tmp = $h*20 + floor($m/3) ;
  //possible indexes , 1 2 3... 
  //max index - 23*20 + (59) /3 = 460 +19 =479 ;
- //$tmp = ($tmp-1)*10 +1 ;
+ $tmp = ($tmp-1)*10 +1 ;
  //indexes  , 1 11 21 31..... ..max = 4781
 
  try
  {
-$sqlq = "SELECT * FROM games where id=".$tmp.";";
-/*$stmt = $conn->prepare($sqlq);
-$stmt->bindValue(1, $tmp);
-$result =$stmt->execute();*/
-$result =$conn->query($sqlq);
-echo $tmp;
-var_dump($result);
-//$res = $conn->query($sqlq);
-/*if(count($registrants) > 0) 
-{*/
-/*echo "fuck me";
-echo $result['id'];
-echo "fucked";*/
-echo "result<br/>";
-echo $result['seq'];
-/* foreach($registrants as $registrant) 
+$sqlq = "SELECT * FROM games";
+/*$stmt = $conn-
+//$result =$stmt->execut>prepare($sqlq);
+$stmt->bindValue(1, $tmp);*/e();
+$stmt = $conn->query($sqlq);
+$registrants = $stmt->fetchAll(); 
+if(count($registrants) > 0) 
+{
+ foreach($registrants as $registrant) 
 		{
       if($tmp == $registrant['id'])
       echo $registrant['seq'];
-		}*/
-//}
+		}
+}
 
 //sleep time would be 23 hr 59 mins ... => 23*60*60+59*60 = 4971540 seconds
 }
@@ -49,4 +45,3 @@ echo $result['seq'];
 	 die($e);
  }
 ?>
-</pre>
