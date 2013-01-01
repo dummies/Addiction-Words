28839dfd9a71d47be192d7e74f1715d1ba03a8f7
dummies <saisreenivasan@live.in>
2013-01-01 12:47:13 +0530

    over

--- js/rank.php
+++ js/rank.php
@@ -7,8 +7,8 @@
     h2 { font-size: 1.75em; }
     h3 { font-size: 1.2em; }
     table { margin-top: 0.75em; }
-    th { font-size: 1.2em; text-align: left; border: none; padding-left: 0; }
-    td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
+    th { font-size: 20px; text-align: left; border: none; padding-left: 0; }
+    td { font-size: 16px; padding: 0.25em 2em 0.25em 0em; border: 0 none; }
 </style>
 
 </head>
