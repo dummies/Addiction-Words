7e6304498fa5398d5f35fe89a5c0aeaa1a710101
dummies <saisreenivasan@live.in>
2013-01-09 19:01:07 +0530

    g tracking

--- index.php
+++ index.php
@@ -8,6 +8,19 @@
 <link href="styles/play.css" rel="stylesheet" type="text/css">
 <link href="styles/images.css" rel="stylesheet" type="text/css">
 <script src="js/fb.js"> </script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-37535306-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 <script src="//connect.facebook.net/en_US/all.js"></script>
 <style>
 .goup {
